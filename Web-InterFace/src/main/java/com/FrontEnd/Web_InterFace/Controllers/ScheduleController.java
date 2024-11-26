1f9a0f97476189a9a2c7ff730a4db7c2034428a3
Avinash9010 <avinashghogare100@gmail.com>
2024-11-26 14:25:34 +0530

    update

--- Web-InterFace/src/main/java/com/FrontEnd/Web_InterFace/Controllers/ScheduleController.java
+++ Web-InterFace/src/main/java/com/FrontEnd/Web_InterFace/Controllers/ScheduleController.java
@@ -45,6 +45,51 @@ public class ScheduleController{
     {
         log.info("Method Access success");
         List<Schedule> result =scheduleService.getSchedules(dId,date);
+        if(result.isEmpty()){
+            Schedule schedule1 = new Schedule();
+            schedule1.setSlotTime("09:00AM");
+            schedule1.setLimit(0);
+            schedule1.setDId(dId);
+            schedule1.setSlotDate(date);
+            userClient.saveSchedule(schedule1);
+
+            Schedule schedule2 = new Schedule();
+            schedule2.setSlotTime("10:00AM");
+            schedule2.setLimit(0);
+            schedule2.setDId(dId);
+            schedule2.setSlotDate(date);
+            userClient.saveSchedule(schedule2);
+
+            Schedule schedule3 = new Schedule();
+            schedule3.setSlotTime("11:00AM");
+            schedule3.setLimit(0);
+            schedule3.setDId(dId);
+            schedule3.setSlotDate(date);
+            userClient.saveSchedule(schedule3);
+
+            Schedule schedule4 = new Schedule();
+            schedule4.setSlotTime("12:00PM");
+            schedule4.setLimit(0);
+            schedule4.setDId(dId);
+            schedule4.setSlotDate(date);
+            userClient.saveSchedule(schedule4);
+
+            Schedule schedule5 = new Schedule();
+            schedule5.setSlotTime("02:00PM");
+            schedule5.setLimit(0);
+            schedule5.setDId(dId);
+            schedule5.setSlotDate(date);
+            userClient.saveSchedule(schedule5);
+
+            Schedule schedule6 = new Schedule();
+            schedule6.setSlotTime("03:00PM");
+            schedule6.setLimit(0);
+            schedule6.setDId(dId);
+            schedule6.setSlotDate(date);
+            userClient.saveSchedule(schedule6);
+
+            return scheduleService.getSchedules(dId,date);
+        }
         return result;
     }
 
