5c1e704486eac0a5821087b4cfde4be45f9734c6
GhatgeRitesh <riteshghatge12345@gmail.com>
2024-12-09 14:56:12 +0530

    updated merge

--- Web-InterFace/src/main/java/com/FrontEnd/Web_InterFace/WebInterFaceApplication.java
+++ Web-InterFace/src/main/java/com/FrontEnd/Web_InterFace/WebInterFaceApplication.java
@@ -4,12 +4,14 @@ import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @Log
 @EnableFeignClients
+@EnableCaching
 public class WebInterFaceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
