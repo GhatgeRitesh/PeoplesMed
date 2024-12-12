da84ec6dd9c1720cf14985a1f341c807840f8feb
GhatgeRitesh <riteshghatge12345@gmail.com>
2024-12-12 11:51:48 +0530

    update

---
+++ ApiGateway/src/test/java/com/PeoplesMed/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PeoplesMed.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
